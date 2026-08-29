ef06c63b68676112cd7e2a9c33ad79c6f648e5c7
marcioxiomi8815-art <marcioxiomi8815@gmail.com>
2026-08-28 23:23:49 -0300

    Fix formatting issue in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,6 +5,6 @@ window.AKATSUKY_FIREBASE_CONFIG = {
   projectId: "akatsuky-delivery",
   storageBucket: "akatsuky-delivery.firebasestorage.app",
   messagingSenderId: "427943624945",
-  appId: "1:427943624945:web:31f5d8b224fe0e97e92950",
+appId: "1:427943624945:web:31f5d8b224fe0e97e92950" ,
   measurementId: "G-MGMJMFSLFB"
 };
